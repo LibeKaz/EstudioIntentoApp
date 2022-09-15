6df13b4f528ab108eafdc1f01f7d07ce171ac4b0
LibeKaz <li.jara@duocuc.cl>
2022-09-14 21:53:45 -0300

    progreso?

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MisdatosComponent } from '../misdatos/misdatos.component';
+import { CertificacionesComponent } from '../certificaciones/certificaciones.component';
+import { ExperienciaLaboralComponent } from '../experiencia-laboral/experiencia-laboral.component';
 import { HomePage } from './home.page';
+
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children:[
+      {
+        path: 'misdatos',
+        component: MisdatosComponent
+      },
+      {
+        path: 'certificaciones',
+        component: CertificacionesComponent
+      },
+      {
+        path: 'experiencia-laboral',
+        component: ExperienciaLaboralComponent
+      }
+    ]
   }
 ];
 
