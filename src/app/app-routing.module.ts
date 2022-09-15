83c1b0f17638480edefd489f704857acdd77ca60
LibeKaz <li.jara@duocuc.cl>
2022-09-14 21:54:36 -0300

    progreso?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {}
 
 const routes: Routes = [
   {
@@ -15,6 +16,8 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+   
+  
 ];
 
 @NgModule({
